8ce4c71b4acd8432c6262d29cbd6bd05fd826df6
Jack <xinxiang@xinxiangdeMacBook-Pro.local>
2022-01-23 17:35:51 +0800

    0.0.1

---
+++ Example/Pods/Target Support Files/FZComponentsKit/FZComponentsKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FZComponentsKit : NSObject
+@end
+@implementation PodsDummy_FZComponentsKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-FZComponentsKit_Example/Pods-FZComponentsKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FZComponentsKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FZComponentsKit_Example
+@end
